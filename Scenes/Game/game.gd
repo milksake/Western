364224215919fce06c901e0c15664eea8a4ee706
Jorge Núñez <94089914+jphants@users.noreply.github.com>
2026-03-12 18:08:59 -0500

    Enemy interface and base for points

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
 @onready var reload: Node2D = $Reload
-var points : int = 0
+@onready var aim: Area2D = $Aim
+
+var score : int = 0
 func _ready() -> void:
 	pass
 
 
 func _process(delta: float) -> void:
 	pass
-	
 
 func _on_reload_left_action(state: int) -> void:
-	pass # Replace with function body.
+	if state > -1:
+		var overlapping_areas = aim.get_overlapping_areas()
+		if overlapping_areas and overlapping_areas[0].has_method("take_damage"):
+			overlapping_areas[0].take_damage()
+	else:
+		print("No bullet")
 
-func _on_reload_middle_action(state: int) -> void:
-	pass # Replace with function body.
-
-func _on_reload_right_action(state: int) -> void:
+func _on_enemy_is_shot(points: Variant) -> void:
+	score += points
+	$Label.text = str("Score: ", score)
 	pass # Replace with function body.
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 var is_aimed : bool = false
+var points : int = 100
+var health : int = 10
+signal is_shot(points)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,13 +12,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"):
-		take_damage()
 	pass
 
-func take_damage():
+func take_damage(damage: int):
 	if is_aimed:
-		print("You did it!!")
+		health -= damage
+		if check_if_dead():
+			is_shot.emit(points)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
@@ -26,3 +29,8 @@ func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		is_aimed = false
 	pass # Replace with function body.
+
+func check_if_dead() -> bool:
+	if health < 0:
+		return true
+	return false
