2da1f4f7db4ee69a7074c527cd189c3db57608b0
UCSP <caceresmariana443@gmail.com>
2026-03-12 16:53:38 -0500

    reload

---
+++ Scenes/reload.gd
@@ -0,0 +1,89 @@
+extends Node2D
+
+@onready var barrel = $Barrel
+@export var bullets : int = 6
+@export var time : float = 0.05
+
+var space : float = 360.0 / bullets
+var pos = 0
+
+var can_move : bool = true
+var buff : int = 0
+var buff_right : bool = false
+var buff_middle : bool = false
+var buff_left : bool = false
+
+signal left_action(state : int)
+signal middle_action(state : int)
+signal right_action(state : int)
+
+var state : Array[int]
+
+func _ready() -> void:
+	state.resize(bullets)
+
+func move(dir : int):
+	var t = create_tween()
+	t.tween_property(barrel, "rotation_degrees", barrel.rotation_degrees + space * dir, time)
+	can_move = false
+	t.connect("finished", reactivate)
+	
+	pos += dir
+	pos += bullets
+	pos %= bullets
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("roll_up"):
+		if can_move:
+			move(-1)
+		else:
+			buff -= 1
+	if Input.is_action_just_pressed("roll_down"):
+		if can_move:
+			move(+1)
+		else:
+			buff += 1
+	if Input.is_action_just_pressed("click"):
+		if can_move:
+			emit_signal("left_action", state[pos])
+		else:
+			buff_left = true
+	if Input.is_action_just_pressed("middleclick"):
+		if can_move:
+			emit_signal("middle_action", state[pos])
+		else:
+			buff_middle = true
+	if Input.is_action_just_pressed("anticlick"):
+		if can_move:
+			emit_signal("right_action", state[pos])
+		else:
+			buff_right = true
+
+func reactivate():
+	if buff > 0:
+		buff -= 1
+		move(+1)
+	elif buff < 0:
+		buff += 1
+		move(-1)
+	else:
+		can_move = true
+		if buff_left:
+			emit_signal("left_action")
+			buff_left = false
+		if buff_right:
+			emit_signal("right_action")
+			buff_right = false
+		if buff_middle:
+			emit_signal("middle_action")
+			buff_middle = false
+
+
+func _on_left_action(state : int) -> void:
+	print("left", state)
+
+func _on_middle_action(state : int) -> void:
+	print("middle", state)
+
+func _on_right_action(state : int) -> void:
+	print("right", state)
